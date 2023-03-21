282ffdbe8846a201b7d76433c866baf0092768e1
Lorinnio <lorinniofn@gmail.com>
2023-03-21 13:36:51 +0100

    implementing sorting movements

--- script.js
+++ script.js
@@ -58,11 +58,13 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   // in empties html from that container <div class = "movements">
   containerMovements.innerHTML = '';
-  //.textContent = 0
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
         <div class="movements__row">
@@ -179,6 +181,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    //Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -198,6 +213,13 @@ btnClose.addEventListener('click', function (e) {
   }
   //clear input fields
   inputClosePin.value = inputCloseUsername.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(movements, !sorted);
+  sorted = !sorted;
 });
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
